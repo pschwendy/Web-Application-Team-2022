b09422fb26253de9f137e9125d3fb373f95636a4
YnotCode <40368753+YnotCode@users.noreply.github.com>
2022-01-01 20:09:24 -0500

    Initial Setup
    Added a server JS file
    Added a .env file for storing sensitive/environment-dependent data
    Added a public folder for assets
    Installed express and dotenv packages
    Created an index.html for the landing page

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use("/public", express.static("./public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(process.env.PORT_NUM, function(){
+    console.log("Listening on port: " + process.env.PORT_NUM);
+});
