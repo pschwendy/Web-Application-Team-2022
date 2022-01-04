50e891d28554c217cd1c843573a9a731861ca8c5
pschwendy <peterschwende@gmail.com>
2022-01-04 00:04:57 -0500

    Formatting changes + Navbar switch

--- public/components/AttractionCard.js
+++ public/components/AttractionCard.js
@@ -9,7 +9,7 @@ define({
   alt: "Random image",
   render: render(function(host, title, description, link, img, alt){
     return html`
-    <div class="card" style="width: 18rem; margin-top: 20px;">
+    <div class="card mx-4" style="margin-top: 20px;">
     <img src=${host.img} class="card-img-top" alt=${host.alt}>
     <div class="card-body">
       <h5 class="card-title">${host.title}</h5>
