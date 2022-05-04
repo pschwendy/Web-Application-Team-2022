e0e306454fe02074b45e692822c1c0a328818be0
pschwendy <peterschwende@gmail.com>
2022-05-03 22:57:56 -0400

    Trying Again

--- queries.js
+++ queries.js
@@ -36,11 +36,11 @@ class queries {
         console.log(process.env.DATABASE_URL);
         console.log(process.env.NODE_ENV);
         this.pool = new Pool(connectionString);
-        this.pool.connect().catch(e => { 
+        /*this.pool.connect().catch(e => { 
             console.log("-------COULDN'T CONNECT TO DATABASE!-------");
             throw(e); 
         });
-        console.log("-------CONNECTED-------");
+        console.log("-------CONNECTED-------");*/
     } // constructor()
 
     deleteReservation(reservation_data) {
